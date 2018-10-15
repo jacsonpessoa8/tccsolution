ca2e23f444fd50f0c0c6821d10fd431d10f29117
Jacson Pessoa <jacsonpessoa8@gmail.com>
2018-10-15 18:46:03 -0300

    start
    inserindo dados part 1

---
+++ Model/Connection.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Connection{
+	
+	private $conn;
+	
+	public function getConnection(){
+		
+		$this->conn = new mysqli("localhost", "root", "1234", "tccsolution");
+		return $this->conn;
+	}
+	
+	public function close(){
+		
+		$this->conn->close();
+	}
+	
+}
---
+++ Model/Usuario.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Usuario{
+	
+	public $CLASS_NAME ="usuario";
+	public $CLASS_TITTLE ="usuario_";
+	public $id;
+	public $name;
+	public $email;
+	public $login;
+	public $senha;
+	
+}
